7806674b6914a54fcfb48b80c4422f0d08d4c2de
Carlos Hinojosa <carloscience@gmail.com>
2015-08-21 13:28:13 -0700

    moving static files

--- dev_input/forms/views.py
+++ dev_input/forms/views.py
@@ -2,15 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
-from .models import githubUrlA
+#from .models import githubUrlA
 
 def index(request):
-  user_input = githubUrlA.objects
+  #user_input = githubUrlA.objects
+  user_input = ""
   template = loader.get_template('forms/index.html')
   context = RequestContext(request, {
     'user_input': user_input,
   })
-  #return HttpResponse("Hello, world. You're at the forms index.")
   return HttpResponse(template.render(context))
 
 def inputUrl(user, repository):
